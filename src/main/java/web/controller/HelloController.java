f5110c8bd088c9e9501baf1c88e26cc87e7d3d27
Rita M <kolotvinova03@bk.ru>
2023-10-16 17:37:26 +0300

    add controller for index

---
+++ src/main/java/web/controller/HelloController.java
@@ -0,0 +1,11 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/index")
+    public String hello(){ return "index"; }
+
+}
